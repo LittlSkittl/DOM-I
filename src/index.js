559f5d51c84407e39b9bf23b0a71fc3149035e0d
Isaac Malin <Isaacmalin3530@gmail.com>
2023-06-19 15:56:28 -0600

    fun exercise and completed website in full

--- src/index.js
+++ src/index.js
@@ -39,4 +39,74 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
+const linkTags = document.querySelectorAll('nav a')
+const firstLink = document.querySelector('a')
+const secondLink = firstLink.nextElementSibling
+const thirdLink = secondLink.nextElementSibling
+const fourthLink = thirdLink.nextElementSibling
+const fifthLink = fourthLink.nextElementSibling
+const sixthLink = fifthLink.nextElementSibling
+
+const greatIdeaLogo = document.querySelector('img')
+
+const firstSectionH1 = document.querySelector('.cta h1') 
+const firstSectionButton = document.querySelector('.cta button')
+const ctaImg= document.querySelector('#cta-img')
+
+const codeImg = document.querySelector('.middle-img')
+
+const textContentH4 = document.querySelector('.text-content h4')
+const textContentP = document.querySelector('.text-content p')
+const secondTextContentH4 = document.querySelector('.text-content:nth-of-type(2) h4')
+const secondTextContentP = document.querySelector('.text-content:nth-of-type(2) p')
+const thirdTextContentH4 = document.querySelector('.text-content:nth-of-type(3) h4')
+const thirdTextContentP = document.querySelector('.text-content:nth-of-type(3) p')
+const fourthTextContentH4 = document.querySelector('.bottom-content .text-content h4')
+const fourthTextContentP = document.querySelector('.bottom-content .text-content p')
+const fifthTextContentH4 = document.querySelector('.bottom-content .text-content:nth-of-type(2) h4')
+const fifthTextContentP = document.querySelector('.bottom-content .text-content:nth-of-type(2) p')
+
+const footerH4 = document.querySelector('.contact h4')
+const footerFirstP = footerH4.nextElementSibling
+const footerSecondP = footerFirstP.nextElementSibling
+const footerThirdP = footerSecondP.nextElementSibling
+const footerLink = document.querySelector('footer a')
+
+
+
+linkTags.forEach(link => {
+  link.classList.add('italic')
+})
+
+firstLink.textContent = siteContent['nav']['nav-item-1']
+secondLink.textContent = siteContent['nav']['nav-item-2']
+thirdLink.textContent = siteContent['nav']['nav-item-3']
+fourthLink.textContent = siteContent['nav']['nav-item-4']
+fifthLink.textContent = siteContent['nav']['nav-item-5']
+sixthLink.textContent = siteContent['nav']['nav-item-6']
+
+greatIdeaLogo.src = 'http://localhost:9000/img/logo.png'
+ctaImg.src = "http://localhost:9000/img/cta.png"
+codeImg.src = "http://localhost:9000/img/accent.png"
+
+firstSectionH1.textContent = siteContent['cta']['h1']
+firstSectionButton.textContent = siteContent['cta']['button']
+
+textContentH4.textContent = siteContent['main-content']['features-h4']
+textContentP.textContent = siteContent['main-content']['features-content']
+secondTextContentH4.textContent = siteContent['main-content']['about-h4']
+secondTextContentP.textContent = siteContent['main-content']['about-content']
+thirdTextContentH4.textContent = siteContent['main-content']['services-h4']
+thirdTextContentP.textContent = siteContent['main-content']['services-content']
+fourthTextContentH4.textContent = siteContent['main-content']['product-h4']
+fourthTextContentP.textContent = siteContent['main-content']['product-content']
+fifthTextContentH4.textContent = siteContent['main-content']['vision-h4']
+fifthTextContentP.textContent = siteContent['main-content']['vision-content']
+
+footerH4.textContent = siteContent['contact']['contact-h4']
+footerFirstP.textContent = siteContent['contact']['address']
+footerSecondP.textContent = siteContent['contact']['phone']
+footerThirdP.textContent = siteContent['contact']['email']
+footerLink.textContent = siteContent['footer']['copyright']
+
+
